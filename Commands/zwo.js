5739eb3a1201bb519c7ffc4111dfb368da8ba62f
Zwo <89267701+Zwobuaq@users.noreply.github.com>
2022-03-03 22:46:57 +0300

    Create zwo.js

---
+++ Commands/zwo.js
@@ -0,0 +1,8 @@
+module.exports = {// Zwobuaq
+
+  "code": "zwo","aliases": [], async run (client,message,args){// Zwobuaq
+
+  message.channel.send('Zwobuaq '+client.ws.ping+'ms');// Zwobuaq
+
+}};// Zwobuaq
+
